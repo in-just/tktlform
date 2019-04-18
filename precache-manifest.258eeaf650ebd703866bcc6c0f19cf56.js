4c60a622a444d40dc6b45304eff9b4e55bc986b0
Justin J. Chou <jjchou@lib.uci.edu>
2019-04-18 16:29:57 -0700

    Initial commit

---
+++ precache-manifest.258eeaf650ebd703866bcc6c0f19cf56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39219b337f0a4f37128e",
+    "url": "/static/css/main.32681b83.chunk.css"
+  },
+  {
+    "revision": "39219b337f0a4f37128e",
+    "url": "/static/js/main.b79dfdd0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "aae25d9482b44a720690",
+    "url": "/static/css/2.f053e07e.chunk.css"
+  },
+  {
+    "revision": "aae25d9482b44a720690",
+    "url": "/static/js/2.56d5f1f9.chunk.js"
+  },
+  {
+    "revision": "cf667d3f3f49ea034b2b5a1983ee81e0",
+    "url": "/index.html"
+  }
+];
